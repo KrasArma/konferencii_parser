0afebf9c597644ccb55348e69f4ffe32f106b9c9
KrasArma <101500808+KrasArma@users.noreply.github.com>
2023-06-07 23:18:55 +0300

    Update test5.py

--- test5.py
+++ test5.py
@@ -7,4 +7,11 @@ url = 'https://konferencii.ru/list?search%5Bkeywords%5D=' + quote(res)
 print(url)
 html = urlopen(url).read().decode('utf-8')
 date = re.findall(r'\d{2}\s\w+\s\d{4}\s\S{2}\s+\S\s+\d{2}\s\w+\s\d{4}\s\S{2}', html)
-print(date)
+start_date = []
+end_date = []
+for i in date:
+    f = i.split("—\n")
+    start_date.append(f[0])
+    end_date.append(f[1])
+df_res = pd.DataFrame({'start_date' : start_date, 'end_date': end_date})
+print(df_res)
